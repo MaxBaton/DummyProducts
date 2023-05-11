287000c1d5fc363e97fdf0ee195268da93657da6
MaximBay <maks.bay.2015@mail.ru>
2023-05-11 22:03:45 +0300

    config retrofit instance

--- app/src/main/java/com/example/dummyproducts/data/retrofit/AppRetrofit.kt
+++ app/src/main/java/com/example/dummyproducts/data/retrofit/AppRetrofit.kt
@@ -1,38 +1,42 @@
 package com.example.dummyproducts.data.retrofit
 
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
-class AppRetrofit {
-    companion object {
-        private var INSTANCE: Retrofit? = null
-        private const val BASE_URL = "https://dummyjson.com"
-        private const val CONNECT_TIMEOUT: Long = 3
-        private const val READ_WRITE_TIMEOUT: Long = 5
+object AppRetrofit {
+    private var INSTANCE: Retrofit? = null
+    private const val BASE_URL = "https://dummyjson.com"
+    private const val CONNECT_TIMEOUT: Long = 5
+    private const val READ_WRITE_TIMEOUT: Long = 5
 
-        fun getRetrofit(): Retrofit {
-            if (INSTANCE == null) {
-                synchronized(AppRetrofit::class.java) {
-                    if (INSTANCE == null) {
-                        val okHttpClient = OkHttpClient().newBuilder()
-                            .connectTimeout(CONNECT_TIMEOUT, TimeUnit.SECONDS)
-                            .readTimeout(READ_WRITE_TIMEOUT, TimeUnit.SECONDS)
-                            .writeTimeout(READ_WRITE_TIMEOUT, TimeUnit.SECONDS)
-                            .build()
-
-
-                        INSTANCE = Retrofit.Builder()
-                            .baseUrl(BASE_URL)
-                            .client(okHttpClient)
-                            .addConverterFactory(GsonConverterFactory.create())
-                            .build()
+    fun getRetrofit(): Retrofit {
+        if (INSTANCE == null) {
+            synchronized(AppRetrofit::class.java) {
+                if (INSTANCE == null) {
+                    val interceptor = HttpLoggingInterceptor().apply {
+                        level = HttpLoggingInterceptor.Level.BODY
                     }
+
+                    val okHttpClient = OkHttpClient().newBuilder()
+                        .connectTimeout(CONNECT_TIMEOUT, TimeUnit.SECONDS)
+                        .readTimeout(READ_WRITE_TIMEOUT, TimeUnit.SECONDS)
+                        .writeTimeout(READ_WRITE_TIMEOUT, TimeUnit.SECONDS)
+                        .addInterceptor(interceptor)
+                        .build()
+
+
+                    INSTANCE = Retrofit.Builder()
+                        .baseUrl(BASE_URL)
+                        .client(okHttpClient)
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .build()
                 }
             }
-
-            return INSTANCE!!
         }
+
+        return INSTANCE ?: throw Exception("Error creating Retrofit instance")
     }
 }
